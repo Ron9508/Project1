242afb36d23b591dec9fcad6f285a2150d8dc41f
unknown <refaatnashabeh@gmail.com>
2025-11-23 07:55:14 +0200

    booksData

---
+++ src/data/booksData.js
@@ -0,0 +1,26 @@
+export const books = [
+  {
+    id: 1,
+    title: "Atomic Habits",
+    author: "James Clear",
+    category: "Self-help",
+    price: 12.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL.jpg"
+  },
+  {
+    id: 2,
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt",
+    category: "Programming",
+    price: 32.50,
+    image: "https://m.media-amazon.com/images/I/41as+WafrFL.jpg"
+  },
+  {
+    id: 3,
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    category: "Finance",
+    price: 10.99,
+    image: "https://m.media-amazon.com/images/I/81bsw6fnUiL.jpg"
+  }
+];
